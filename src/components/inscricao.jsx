41722f3098aac70aa0683d702eaa446e2891b593
Filipe Lopes <filiperocklopes@gmail.com>
2019-02-04 23:27:27 -0300

    Implementação de 'Termos de participação no formulário de inscrição' com envio condicional à leitura através do botão 'Li e aceito'

--- src/components/inscricao.jsx
+++ src/components/inscricao.jsx
@@ -7,23 +7,10 @@ class Inscricao extends Component {
     super(props);
 
     this.state = {
-      data: [ {
-        name: "Carregando...",
-        slug: "carregando",
-        description: "carregando...",
-        items: [
-          { type : "cover", url: "carregando.jpg"}
-        ],
-        categories: [ "carregando..." ],
-        tags: [ "carregando..." ]
-      }],
-      atual: {
-        items: []
-      },
-      pixel: 0,
-      backgroundColor: "#252525",
-      currentIndex: 0
+      canSend: false
     }
+
+    this.submitBt = React.createRef();
 
   }
 
@@ -46,6 +33,12 @@ class Inscricao extends Component {
     }); 
   }
 
+  canSend = (e) => {
+    this.setState({canSend:e.target.checked}, () => {
+      this.submitBt.current.disabled = this.state.canSend ? false : true
+    })
+  }
+
   render () {
     return (
       <div className="page">
@@ -59,17 +52,26 @@ class Inscricao extends Component {
               </section>
               <section>
               <form encType="multipart/form-data" method="POST" onSubmit={this.sendPhoto}>
-                <label htmlFor="nome">Nome</label>
-                <input type="text" name="nome" id="nome" required />
+                <label htmlFor="nome">Nome do colaborador</label>
+                <input type="text" name="nome" id="nome" placeholder="Insira o seu nome" required />
                 <label htmlFor="nome-foto">Nome da Foto</label>
-                <input type="text" name="nome-foto" id="nome-foto" required/>
+                <input type="text" name="nome-foto" id="nome-foto" placeholder="Insira o nome da foto" required/>
                 <label htmlFor="nome-foto">Local da Foto</label>
-                <input type="text" name="local-foto" id="local-foto" required/>
+                <input type="text" name="local-foto" id="local-foto" placeholder="insira o local da foto" required/>
                 <label htmlFor="nome-foto">Data de registro da foto</label>
                 <input type="date" name="data-foto" id="data-foto" required/>
                 <label htmlFor="foto" className="file">Clique aqui para escolher sua foto</label>
                 <input type="file" name="foto" id="foto" required/>
-                <button type="submit">Enviar</button>
+                <label>Termos de participação</label>
+                <div id="termos">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet erat eget nunc ullamcorper elementum ut non lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam nec varius massa. Morbi condimentum pulvinar neque, a gravida odio tincidunt volutpat. Curabitur sit amet quam lorem. Vivamus mollis mi vitae ex tincidunt tincidunt. Nullam fermentum mauris quis consequat dignissim.</p>
+
+                <p>Donec nisl massa, facilisis a leo a, dapibus volutpat ipsum. Nulla interdum augue nec facilisis blandit. Pellentesque rhoncus congue ex sit amet accumsan. Sed accumsan blandit justo, eu placerat odio consequat vel. Duis accumsan eget est quis gravida. Nullam tempus mollis sem at accumsan. Quisque aliquam finibus erat eu pellentesque. Vestibulum nec est massa. Nulla facilisi. Aliquam nec laoreet massa. Nam erat urna, posuere a elementum eget, dictum eu magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin interdum tellus nulla, ut dictum dolor consectetur vel.</p>
+
+                <p>Sed nunc augue, luctus a tristique vel, venenatis ut dolor. Praesent consequat malesuada consequat. Cras blandit massa finibus, porta erat vel, bibendum magna. Integer luctus eros eu ante vehicula, quis cursus odio venenatis. Nulla ligula sem, molestie sit amet orci in, finibus tristique sapien. Quisque tempor enim nunc. Maecenas ut ultrices magna. Curabitur vestibulum, dui sed lacinia blandit, nisi sem pharetra augue, sit amet interdum metus tortor in dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur gravida justo velit, facilisis scelerisque arcu aliquet sed. Duis efficitur libero mi, non tincidunt ligula dignissim ac. Vestibulum non neque interdum, faucibus turpis sit amet, elementum ipsum. Integer scelerisque dictum rutrum.</p>
+                </div><br/>
+                <input type="checkbox" onChange={this.canSend}/> <strong>Li e concordo com os termos de participação</strong><br/><br/>
+                <button ref={this.submitBt} type="submit" disabled>Enviar</button>
               </form>
               </section>
             </div>
